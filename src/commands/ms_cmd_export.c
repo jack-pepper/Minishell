c71be13e0d518c62b03ac60b0f1448ef703235f2
Morgan Malie <mmalie@c2r6p6.42nice.fr>
2025-04-03 12:51:31 +0200

    implement a working version of cmd export (partial parsing, can only add vars, does not check for duplicate yet)

--- src/commands/ms_cmd_export.c
+++ src/commands/ms_cmd_export.c
@@ -6,7 +6,7 @@
 /*   By: mmalie <mmalie@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 13:03:40 by mmalie            #+#    #+#             */
-/*   Updated: 2025/04/02 22:53:53 by mmalie           ###   ########.fr       */
+/*   Updated: 2025/04/03 11:44:15 by mmalie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 
 void	cmd_export(char **input_args, t_list **this_env)
 {
-	printf("[cmd_export]\n");
+	t_list	*new_var;
+	char	*equal_appended;
+	int	i;
+	int	nb_of_equal;
+
+	new_var = NULL;
 	if (!input_args)
 		return ;
 	if (!(input_args[1])) // if no args, man says result is unspecified
@@ -22,37 +27,30 @@ void	cmd_export(char **input_args, t_list **this_env)
 		cmd_env((*this_env)); // ... we may just display the env variables
 		return ;
 	}
-	
-	// TEST
-	else   // ADD NODE TO LIST
-	{
-
-		//size_t i = ft_strslen(this_env);
-
-
-	/*	new_env = malloc(sizeof(char *) * (i + 2)); // replace the number by the number of env var to add + 1
-		if (!new_env)
-			return ;
-		ft_memmove(new_env, this_env, sizeof(char *) * i);
-	
-		new_env[i] = ft_strdup("ZZZ=aaa");
-		if (!new_env[i])
-			return ;
-		new_env[i + 1] = NULL;
-		this_env = new_env;
-	*/
-
-	}
-
-	/*
 	else
 	{
-		equal_sign = ft_strchr(input_args[1], '=');
-		if (equal_sign == NULL)
+		i = 1;
+		while (input_args[i]) // parsing probably uncomplete
 		{
-			//
+			// SHOULD CHECK whether the var already exists, and update it accordingly
+			nb_of_equal = ft_count_char(input_args[i], '=');
+			if ((input_args[i][0] != '=') // first char not an equal
+				&& (nb_of_equal <= 1)) // can't be more than one equal
+			{
+				if (nb_of_equal == 0)
+				{
+					equal_appended = ft_strjoin(input_args[i], "=");
+					if (!equal_appended)
+						return ;	
+					new_var = ft_lstnew(((char *)equal_appended));
+				}
+				else
+					new_var = ft_lstnew((char *)input_args[i]);
+				if (!new_var)
+					return ;
+				ft_lstadd_back(this_env, new_var);
+			}
+			i++;
 		}
 	}
-	// ...
-	*/
 }
