cdc98a82b1cdfce8cc3696a7e49881e3e2290d1e
Lepokile <yahya.elboukili1@gmail.com>
2025-06-16 13:38:58 -0400

    mkdir a/b/c, rm -rf, now working properly. Missing norminette

--- src/builtins/cd/ms_cmd_cd.c
+++ src/builtins/cd/ms_cmd_cd.c
@@ -6,11 +6,58 @@
 /*   By: yel-bouk <yel-bouk@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/24 00:03:54 by mmalie            #+#    #+#             */
-/*   Updated: 2025/06/16 11:20:50 by yel-bouk         ###   ########.fr       */
+/*   Updated: 2025/06/16 13:35:02 by yel-bouk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../inc/minishell.h"
+
+static int	handle_cd_path_failure(char *user_path)
+{
+	free(user_path);
+	return (1);
+}
+
+static char	*fallback_to_pwd_if_cwd_fails(t_shell *sh)
+{
+	t_list	*pwd_var;
+	char	*cwd;
+
+	pwd_var = ft_getenv("PWD", &sh->this_env);
+	if (pwd_var != NULL && ((char **)pwd_var->content)[1])
+		cwd = ft_strdup(((char **)pwd_var->content)[1]);
+	else
+		cwd = NULL;
+	return (cwd);
+}
+
+int	cmd_cd(t_shell *sh)
+{
+	char	*cwd;
+	char	*path;
+	t_list	*home_var;
+	int		res;
+	char	*user_path;
+
+	home_var = ft_getenv("HOME", &sh->this_env);
+	path = NULL;
+	res = cd_set_path(sh, home_var, &path);
+	if (res < 2)
+		return (res);
+	user_path = ft_strdup(path);
+	cwd = store_cwd(NULL);
+	if (!cwd)
+	{
+		cwd = fallback_to_pwd_if_cwd_fails(sh);
+		if (!cwd)
+			return (ms_err("cd", "", PWD_NON_SET, 1));
+	}
+	if (cd_process_path(sh, cwd, path, user_path) != 0)
+		return (handle_cd_path_failure(user_path));
+	free(user_path);
+	free(cwd);
+	return (0);
+}
 
 char	*join_parts(char **parts)
 {
@@ -42,48 +89,51 @@ char	*join_parts(char **parts)
 	return (res);
 }
 
+static int	append_and_replace(char **dst, char *suffix)
+{
+	char	*temp;
+
+	temp = ft_strjoin(*dst, suffix);
+	if (!temp)
+		return (0);
+	free(*dst);
+	*dst = temp;
+	return (1);
+}
+
+static void	update_if_exists(char **last_existing, char *current)
+{
+	if (access(current, F_OK) == 0)
+	{
+		free(*last_existing);
+		*last_existing = ft_strdup(current);
+	}
+}
+
 char	*find_last_existing_dir(char *path)
 {
 	char	**path_parts;
 	char	*current_path;
-	char	*last_existing = NULL;
+	char	*last_existing;
 	int		i;
-	char	*temp;
 
-	path_parts = ft_split(path, '/');
-	if (!path_parts)
-		return (NULL);
+	i = 0;
+	last_existing = NULL;
 	current_path = ft_strdup("/");
-	if (!current_path)
+	path_parts = ft_split(path, '/');
+	if (!path_parts || !current_path)
 	{
 		free_args(path_parts);
+		free(current_path);
 		return (NULL);
 	}
-	i = 0;
 	while (path_parts[i])
 	{
-		temp = ft_strjoin(current_path, path_parts[i]);
-		free(current_path);
-		if (!temp)
-		{
-			free_args(path_parts);
-			return (last_existing);
-		}
-		current_path = temp;
-		if (access(current_path, F_OK) == 0)
-		{
-			if (last_existing)
-				free(last_existing);
-			last_existing = ft_strdup(current_path);
-		}
-		temp = ft_strjoin(current_path, "/");
-		free(current_path);
-		if (!temp)
-		{
-			free_args(path_parts);
-			return (last_existing);
-		}
-		current_path = temp;
+		if (!append_and_replace(&current_path, path_parts[i]))
+			break ;
+		update_if_exists(&last_existing, current_path);
+		if (!append_and_replace(&current_path, "/"))
+			break ;
 		i++;
 	}
 	free(current_path);
@@ -91,88 +141,23 @@ char	*find_last_existing_dir(char *path)
 	return (last_existing);
 }
 
-char	*apply_logical_cd(char *pwd, char *cd_path)
+static char	*merge_pwd_and_path(char **pwd_split, char **path_split)
 {
-	char	**pwd_split;
-	pwd_split = ft_split(pwd, '/');
-	char	**path_split = ft_split(cd_path, '/');
 	char	**result;
 	char	*final_result;
-	int		i = 0, j = 0;
-	static int	cd_up_count = 0;
-	static int	deleted_dirs = 0;
+	int		i;
+	int		j;
 
-	if (!pwd_split || !path_split)
-		return (NULL);
-	if (ft_strcmp(cd_path, "..") == 0)
-	{
-		if (cd_up_count == 0)
-		{
-			char *current_path = ft_strdup("/");
-			if (!current_path)
-			{
-				free_args(pwd_split);
-				free_args(path_split);
-				return (NULL);
-			}
-			i = 0;
-			while (pwd_split[i])
-			{
-				char *temp = ft_strjoin(current_path, pwd_split[i]);
-				free(current_path);
-				if (!temp)
-				{
-					free_args(pwd_split);
-					free_args(path_split);
-					return (NULL);
-				}
-				current_path = temp;
-
-				if (access(current_path, F_OK) != 0)
-					deleted_dirs++;
-
-				temp = ft_strjoin(current_path, "/");
-				free(current_path);
-				if (!temp)
-				{
-					free_args(pwd_split);
-					free_args(path_split);
-					return (NULL);
-				}
-				current_path = temp;
-				i++;
-			}
-			free(current_path);
-		}
-		cd_up_count++;
-		if (cd_up_count >= deleted_dirs)
-		{
-			cd_up_count = 0;
-			deleted_dirs = 0;
-			char *last_existing = find_last_existing_dir(pwd);
-			free_args(pwd_split);
-			free_args(path_split);
-			if (last_existing)
-				return (last_existing);
-			return (ft_strdup("/"));
-		}
-		free_args(pwd_split);
-		free_args(path_split);
-		ft_printf("cd: error retrieving current directory: getcwd: cannot access parent directories: No such file or directory\n");
-		return (ft_strjoin(pwd, "/.."));
-	}
 	result = malloc(sizeof(char *) * 1024);
 	if (!result)
-	{
-		free_args(pwd_split);
-		free_args(path_split);
 		return (NULL);
-	}
+	i = 0;
 	while (pwd_split[i])
 	{
 		result[i] = ft_strdup(pwd_split[i]);
 		i++;
 	}
+	j = 0;
 	while (path_split[j])
 	{
 		if (!ft_strcmp(path_split[j], "..") && i > 0)
@@ -184,118 +169,153 @@ char	*apply_logical_cd(char *pwd, char *cd_path)
 	result[i] = NULL;
 	final_result = join_parts(result);
 	free_args(result);
-	free_args(pwd_split);
-	free_args(path_split);
 	return (final_result);
 }
 
-int	cmd_cd(t_shell *sh)
+static char	*handle_cd_dotdot_case(char *pwd,
+			char **pwd_split, char **path_split)
 {
-	char	*cwd;
-	char	*path;
-	t_list	*home_var;
-	int		res;
-	char	*user_path;
-	t_list	*pwd_var;
+	static int	cd_up_count;
+	static int	deleted_dirs;
+	char		*current_path;
+	int			i;
 
-	home_var = ft_getenv("HOME", &sh->this_env);
-	path = NULL;
-	res = cd_set_path(sh, home_var, &path);
-	if (res < 2)
-		return (res);
-	user_path = ft_strdup(path);
-	cwd = store_cwd(NULL);
-	if (!cwd)
+	i = 0;
+	if (cd_up_count == 0)
 	{
-		pwd_var = ft_getenv("PWD", &sh->this_env);
-		if (pwd_var != NULL && ((char **)pwd_var->content)[1])
-			cwd = ft_strdup(((char **)pwd_var->content)[1]);
-		if (!cwd)
-			return (ms_err("cd", "", PWD_NON_SET, 1));
+		current_path = ft_strdup("/");
+		while (pwd_split[i])
+		{
+			if (!append_and_replace(&current_path, pwd_split[i]))
+				return (NULL);
+			if (access(current_path, F_OK) != 0)
+				deleted_dirs++;
+			if (!append_and_replace(&current_path, "/"))
+				return (NULL);
+			i++;
+		}
+		free(current_path);
 	}
-	if (cd_process_path(sh, cwd, path, user_path) != 0)
+	cd_up_count++;
+	if (cd_up_count >= deleted_dirs)
 	{
-		free(user_path);
-		return (1);
+		cd_up_count = 0;
+		deleted_dirs = 0;
+		free_args(pwd_split);
+		free_args(path_split);
+		return (find_last_existing_dir(pwd));
 	}
-	free(user_path);
-	free(cwd);
-	return (0);
+	free_args(pwd_split);
+	free_args(path_split);
+	ft_printf("cd: error retrieving current directory: getcwd:"
+		"cannot access parent directories: No such file or directory\n");
+	return (ft_strjoin(pwd, "/.."));
 }
 
-char *resolve_logical_path(char *cwd, char *user_path)
+char	*apply_logical_cd(char *pwd, char *cd_path)
+{
+	char	**pwd_split;
+	char	**path_split;
+	char	*result;
+
+	pwd_split = ft_split(pwd, '/');
+	path_split = ft_split(cd_path, '/');
+	if (!pwd_split || !path_split)
+		return (NULL);
+	if (ft_strcmp(cd_path, "..") == 0)
+	{
+		result = handle_cd_dotdot_case(pwd, pwd_split, path_split);
+		return (result);
+	}
+	result = merge_pwd_and_path(pwd_split, path_split);
+	free_args(pwd_split);
+	free_args(path_split);
+	return (result);
+}
+
+char	*resolve_logical_path(char *cwd, char *user_path)
 {
 	if (!cwd || !user_path)
 		return (NULL);
-	return apply_logical_cd(cwd, user_path);
+	return (apply_logical_cd(cwd, user_path));
+}
+
+static int	try_logical_cd(t_shell *sh, char *cwd, char *user_path)
+{
+	char	*logical_path;
+	char	*new_real;
+	char	*simulated;
+
+	logical_path = resolve_logical_path(cwd, user_path);
+	if (!logical_path)
+		return (1);
+	if (chdir(logical_path) != 0)
+	{
+		simulated = ft_strjoin(cwd, "/..");
+		if (!simulated)
+		{
+			free(logical_path);
+			return (1);
+		}
+		update_pwds_vars(sh, cwd, simulated);
+		free(simulated);
+		free(logical_path);
+		return (0);
+	}
+	new_real = store_cwd(NULL);
+	if (!new_real)
+	{
+		simulated = ft_strjoin(cwd, "/..");
+		update_pwds_vars(sh, cwd, simulated);
+		free(simulated);
+	}
+	else
+	{
+		update_pwds_vars(sh, cwd, new_real);
+		free(new_real);
+	}
+	free(logical_path);
+	return (0);
+}
+
+static int	handle_relative_cd(t_shell *sh, char *cwd,
+		char *path, char *user_path)
+{
+	char	*cur_path;
+	char	*real_path;
+
+	cur_path = handle_dotted_path(cwd, path);
+	if (cur_path && chdir(cur_path) == 0)
+	{
+		real_path = store_cwd(NULL);
+		if (real_path)
+		{
+			update_pwds_vars(sh, cwd, real_path);
+			free(real_path);
+		}
+		else
+			update_pwds_vars(sh, cwd, cur_path);
+		free(cur_path);
+		return (0);
+	}
+	free(cur_path);
+	return (try_logical_cd(sh, cwd, user_path));
 }
 
 int	cd_process_path(t_shell *sh, char *cwd, char *path, char *user_path)
 {
-	char			*cur_path;
-	char			*logical_path;
+	t_list	*oldpwd_var;
 
-	cur_path = NULL;
-	logical_path = NULL;
 	if (path[0] == '.')
+		return (handle_relative_cd(sh, cwd, path, user_path));
+	if (ft_strcmp(path, "-") == 0)
 	{
-		cur_path = handle_dotted_path(cwd, path);
-		if (cur_path && chdir(cur_path) == 0)
-		{
-			char *real_path = store_cwd(NULL);
-			if (real_path)
-			{
-				update_pwds_vars(sh, cwd, real_path);
-				free(real_path);
-			}
-			else
-				update_pwds_vars(sh, cwd, cur_path);
-			free(cur_path);
-			return (0);
-		}
-		free(cur_path);
-		logical_path = resolve_logical_path(cwd, user_path);
-		if (!logical_path)
-			return (1);
-		if (chdir(logical_path) != 0)
-		{
-			char *simulated = ft_strjoin(cwd, "/..");
-			if (!simulated)
-			{
-				free(logical_path);
-				return (1);
-			}
-			update_pwds_vars(sh, cwd, simulated);
-			free(simulated);
-			free(logical_path);
-			return (0);
-		}
-		char *new_real = store_cwd(NULL);
-		if (!new_real)
-		{
-			char *simulated = ft_strjoin(cwd, "/..");
-			update_pwds_vars(sh, cwd, simulated);
-			free(simulated);
-		}
-		else
-		{
-			update_pwds_vars(sh, cwd, new_real);
-			free(new_real);
-		}
-		free(logical_path);
-		return (0);
+		oldpwd_var = ft_getenv("OLDPWD", &sh->this_env);
+		if (!oldpwd_var)
+			return (ms_err("cd", "", OLDPWD_NON_SET, 1));
+		path = ((char **)oldpwd_var->content)[1];
 	}
-	else
-	{
-		if (ft_strcmp(path, "-") == 0)
-		{
-			t_list *oldpwd_var = ft_getenv("OLDPWD", &sh->this_env);
-			if (!oldpwd_var)
-				return ms_err("cd", "", OLDPWD_NON_SET, 1);
-			path = ((char **)oldpwd_var->content)[1];
-		}
-		return (change_directory(sh, cwd, path, user_path));
-	}
+	return (change_directory(sh, cwd, path, user_path));
 }
 
 int	change_directory(t_shell *sh, char *cwd, char *path, char *user_path)
